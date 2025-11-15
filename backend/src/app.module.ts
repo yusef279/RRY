c701c701652ae0561f54e6096e5319d64a9ff1ad
Rowaida <rowaidamokhtar96@gmail.com>
2025-11-15 15:18:18 +0200

    fixed dependencies

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,6 +1,5 @@
 import { Module } from '@nestjs/common';
-impnpm install @nestjs/config
-ort { ConfigModule } from '@nestjs/config';
+import { ConfigModule } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
 import { EmployeeModule } from './employee/employee.module';
 import { PerformanceModule } from './performance/performance.module';
