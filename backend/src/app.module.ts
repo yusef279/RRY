cd40505938e06fc232884a315f25b6819bd67058
Rana Abouraia <16abourr@gmail.com>
2025-11-21 18:31:32 +0200

    .env

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -3,6 +3,7 @@ import { ConfigModule } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
 import { EmployeeModule } from './employee/employee.module';
 import { PerformanceModule } from './performance/performance.module';
+import { OrgStructureModule } from './org-structure/org-structure.module';
 
 @Module({
   imports: [
@@ -11,6 +12,7 @@ import { PerformanceModule } from './performance/performance.module';
       process.env.MONGO_URI ?? 'mongodb://127.0.0.1:27017/hr-system',
     ),
     EmployeeModule,
+    OrgStructureModule,
     PerformanceModule,
   ],
 })
